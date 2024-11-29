a063d89dce9ca7aa4fc3b95ccc2582d5ea8d94b2
morrs <grishkovea@gmail.com>
2024-11-29 23:29:29 +0300

    init commit

---
+++ src/main/java/org/morrs/circusbackend/CircusBackendApplication.java
@@ -0,0 +1,13 @@
+package org.morrs.circusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircusBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/morrs/circusbackend/CircusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.morrs.circusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircusBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
