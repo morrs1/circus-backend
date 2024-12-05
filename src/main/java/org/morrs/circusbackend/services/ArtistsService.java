afc6282bd2795cde64b3e8fc43cda51374d2cbe4
morrs <grishkovea@gmail.com>
2024-12-06 01:55:00 +0300

    feat: добавил сервис для артиста

---
+++ src/main/java/org/morrs/circusbackend/services/ArtistsService.java
@@ -0,0 +1,42 @@
+package org.morrs.circusbackend.services;
+
+import lombok.AllArgsConstructor;
+import org.morrs.circusbackend.models.Artist;
+import org.morrs.circusbackend.repo.ArtistsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+@Transactional(readOnly = true)
+public class ArtistsService {
+    private ArtistsRepository artistsRepository;
+
+    public List<Artist> readAll() {
+        return artistsRepository.findAll();
+    }
+
+    public Artist read(int id) {
+        return artistsRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public void update(Artist artist, int id) {
+        artist.setArtistNum(id);
+        artistsRepository.save(artist);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        artistsRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void save(Artist artist) {
+        artistsRepository.save(artist);
+    }
+
+}
