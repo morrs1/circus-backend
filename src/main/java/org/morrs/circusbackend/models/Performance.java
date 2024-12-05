948aa35c1c06becec2167367eea0da40470616fd
morrss <grishkovea@gmail.com>
2024-12-05 17:23:23 +0300

    feat: добавил сущность для представления

---
+++ src/main/java/org/morrs/circusbackend/models/Performance.java
@@ -0,0 +1,37 @@
+package org.morrs.circusbackend.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Table(name = "performances")
+@Data
+public class Performance {
+
+    @Id
+    @Column(name = "per_code")
+    private Integer perCode;
+
+    @Column()
+    private String name;
+
+    @Column()
+    private LocalDate date;
+
+    @Column()
+    private LocalTime time;
+
+    @ManyToMany
+    @JoinTable(
+            name = "artists_performances",
+            joinColumns = @JoinColumn(name = "per_code"),
+            inverseJoinColumns = @JoinColumn(name = "artist_num")
+    )
+    @ToString.Exclude
+    private List<Artist> artists;
+}
