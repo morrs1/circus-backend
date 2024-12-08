e98b6a51ca5a1c6fe33765a29cf429f62233ee2f
morrs <grishkovea@gmail.com>
2024-12-09 00:43:28 +0300

    feat: добавил ручки для чтения всех артистов и обновления артиста

--- src/main/java/org/morrs/circusbackend/controllers/AdminController.java
+++ src/main/java/org/morrs/circusbackend/controllers/AdminController.java
@@ -5,7 +5,11 @@ import org.morrs.circusbackend.models.Artist;
 import org.morrs.circusbackend.services.ArtistsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/admin")
@@ -24,9 +28,32 @@ public class AdminController {
         return "admin/create";
     }
 
-    @PostMapping("/123")
+    @PostMapping("")
     public String create(@ModelAttribute("artist") Artist artist) {
         artistsService.save(artist);
+        return "admin/index-admin";
+    }
+
+    @GetMapping("read-all")
+    public String readAll(Model model) {
+        model.addAttribute("artists", artistsService.readAll());
+        return "admin/read-all";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String updatePage(Model model, @PathVariable UUID id) {
+        var artist = artistsService.read(id);
+        model.addAttribute("artist", artist);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String formattedBirthDate = artist.getBirthDate() != null ? artist.getBirthDate().format(formatter) : "";
+        model.addAttribute("formattedBirthDate", formattedBirthDate);
+        System.out.println(formattedBirthDate);
+        return "admin/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("artist") Artist artist, @PathVariable UUID id) {
+        artistsService.update(artist, id);
         return "admin/index-admin";
     }
 
