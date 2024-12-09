5182d53a4784da79399f9dd61c9a9955d20ec336
morrs <grishkovea@gmail.com>
2024-12-09 19:46:08 +0300

    fix: изменил запрос, тк прошлый некорректно выводил данные

--- src/main/java/org/morrs/circusbackend/repo/TicketsReportRepository.java
+++ src/main/java/org/morrs/circusbackend/repo/TicketsReportRepository.java
@@ -18,34 +18,32 @@ public class TicketsReportRepository {
     public List<TicketsReport> getReport() {
         String sql =
                 """
-                        SELECT p.per_code                                             AS performanceCode,
-                               COALESCE(sold_tickets.count_sold_tickets, 0)           AS amountOfSoldTickets,
-                               COALESCE(available_tickets.count_available_tickets, 0) AS amountOfFreeTickets,
-                               CASE
-                                   WHEN COALESCE(available_tickets.count_available_tickets, 0) = 0 THEN 0
-                                   ELSE
-                                       CASE
-                                           WHEN COALESCE(sold_tickets.count_sold_tickets, 0) * 1.0 / available_tickets.count_available_tickets <
-                                                0.7 THEN 0
-                                           ELSE COALESCE(sold_tickets.count_sold_tickets, 0) * 1.0 / available_tickets.count_available_tickets
-                                           END
-                                   END                                                AS ratioSoldTicketsToFreeTickets
-                        FROM performances p
-                                 LEFT JOIN
-                             (SELECT t.performance_code,
-                                     COUNT(t.ticket_code) - COUNT(ts.num_sale) AS count_available_tickets
-                              FROM tickets t
-                                       LEFT JOIN
-                                   tickets_sale ts ON t.num_sale = ts.num_sale
-                              GROUP BY t.performance_code) AS available_tickets ON p.per_code = available_tickets.performance_code
-                                 LEFT JOIN
-                             (SELECT t.performance_code,
-                                     COUNT(ts.num_sale) AS count_sold_tickets
-                              FROM tickets t
-                                       JOIN
-                                   tickets_sale ts ON t.num_sale = ts.num_sale
-                              GROUP BY t.performance_code) AS sold_tickets ON p.per_code = sold_tickets.performance_code;
-                        """;
+                        SELECT p.per_code AS performanceCode,
+                                                   COALESCE(sold_tickets.count_sold_tickets, 0) AS amountOfSoldTickets,
+                                                   COALESCE(available_tickets.count_available_tickets, 0) AS amountOfFreeTickets,
+                                                   CASE
+                                                       WHEN COALESCE(available_tickets.count_available_tickets, 0) + COALESCE(sold_tickets.count_sold_tickets, 0) = 0 THEN 0
+                                                       ELSE
+                                                           CASE
+                                                               WHEN COALESCE(sold_tickets.count_sold_tickets, 0) * 1.0 /\s
+                                                                    (COALESCE(sold_tickets.count_sold_tickets, 0) + COALESCE(available_tickets.count_available_tickets, 0)) > 0.7 THEN 0
+                                                               ELSE 1
+                                                           END
+                                                   END AS ratioSoldTicketsToFreeTickets
+                                            FROM performances p
+                                            LEFT JOIN
+                                                (SELECT t.performance_code,
+                                                        COUNT(t.ticket_code) - COUNT(ts.num_sale) AS count_available_tickets
+                                                 FROM tickets t
+                                                 LEFT JOIN tickets_sale ts ON t.num_sale = ts.num_sale
+                                                 GROUP BY t.performance_code) AS available_tickets ON p.per_code = available_tickets.performance_code
+                                            LEFT JOIN
+                                                (SELECT t.performance_code,
+                                                        COUNT(ts.num_sale) AS count_sold_tickets
+                                                 FROM tickets t
+                                                 JOIN tickets_sale ts ON t.num_sale = ts.num_sale
+                                                 GROUP BY t.performance_code) AS sold_tickets ON p.per_code = sold_tickets.performance_code;
+                    """;
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(TicketsReport.class));
     }
 
--- src/main/resources/reports.sql
+++ src/main/resources/reports.sql
@@ -37,30 +37,28 @@ FROM artists a
 
 
 
-SELECT p.per_code                                             AS performanceCode,
-       COALESCE(sold_tickets.count_sold_tickets, 0)           AS amountOfSoldTickets,
+SELECT p.per_code AS performanceCode,
+       COALESCE(sold_tickets.count_sold_tickets, 0) AS amountOfSoldTickets,
        COALESCE(available_tickets.count_available_tickets, 0) AS amountOfFreeTickets,
        CASE
-           WHEN COALESCE(available_tickets.count_available_tickets, 0) = 0 THEN 0
+           WHEN COALESCE(available_tickets.count_available_tickets, 0) + COALESCE(sold_tickets.count_sold_tickets, 0) = 0 THEN 0
            ELSE
                CASE
-                   WHEN COALESCE(sold_tickets.count_sold_tickets, 0) * 1.0 / available_tickets.count_available_tickets <
-                        0.7 THEN 0
-                   ELSE COALESCE(sold_tickets.count_sold_tickets, 0) * 1.0 / available_tickets.count_available_tickets
+                   WHEN COALESCE(sold_tickets.count_sold_tickets, 0) * 1.0 /
+                        (COALESCE(sold_tickets.count_sold_tickets, 0) + COALESCE(available_tickets.count_available_tickets, 0)) > 0.7 THEN 1
+                   ELSE 0
                    END
-           END                                                AS ratioSoldTicketsToFreeTickets
+           END AS ratioSoldTicketsToFreeTickets
 FROM performances p
          LEFT JOIN
      (SELECT t.performance_code,
              COUNT(t.ticket_code) - COUNT(ts.num_sale) AS count_available_tickets
       FROM tickets t
-               LEFT JOIN
-           tickets_sale ts ON t.num_sale = ts.num_sale
+               LEFT JOIN tickets_sale ts ON t.num_sale = ts.num_sale
       GROUP BY t.performance_code) AS available_tickets ON p.per_code = available_tickets.performance_code
          LEFT JOIN
      (SELECT t.performance_code,
              COUNT(ts.num_sale) AS count_sold_tickets
       FROM tickets t
-               JOIN
-           tickets_sale ts ON t.num_sale = ts.num_sale
+               JOIN tickets_sale ts ON t.num_sale = ts.num_sale
       GROUP BY t.performance_code) AS sold_tickets ON p.per_code = sold_tickets.performance_code;
