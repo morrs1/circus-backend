be797f78824ca6f3aeda92e3b22d09a824877096
morrs <grishkovea@gmail.com>
2024-12-10 02:24:28 +0300

    feat: добавил метод для поиска свободных билетов на представление

--- src/main/java/org/morrs/circusbackend/repo/TicketsRepository.java
+++ src/main/java/org/morrs/circusbackend/repo/TicketsRepository.java
@@ -2,8 +2,14 @@ package org.morrs.circusbackend.repo;
 
 import org.morrs.circusbackend.models.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TicketsRepository extends JpaRepository<Ticket, Integer> {
+
+    @Query(value = "SELECT * FROM tickets WHERE num_sale IS NULL AND performance_code = ?1", nativeQuery = true)
+    List<Ticket> findAllFreeTickets(int performanceCode);
 }
